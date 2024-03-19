3e9ce67adb65b7a4e6d6f2442e28e4f8a2d6d72d
Saud Zahir <m.saud.zahir@gmail.com>
2024-03-20 01:05:36 +0500

    Add CMakeLists.txt

--- install.py
+++ install.py
@@ -2,12 +2,13 @@ import subprocess
 import time
 import shlex
 
-build_cmd = shlex.split("python build.py")
 wheel_cmd = shlex.split("python -m pip wheel . -v -w dist ")
 install_cmd = shlex.split("python -m pip install -e . -v")
 
 with open("build.log", "w") as log:
-    subprocess.run(build_cmd, stdout=log, stderr=log)
+    subprocess.run("cd build", stdout=log, stderr=log)
+    subprocess.run("cmake ..", stdout=log, stderr=log)
+    subprocess.run("make", stdout=log, stderr=log)
 
 time.sleep(2)
 
