d7239dcbbea20dcf4250b22d09083fe400d0e7d2
Saud Zahir <m.saud.zahir@gmail.com>
2024-03-21 22:26:10 +0500

    Add skbuild

--- install.py
+++ install.py
@@ -5,14 +5,8 @@ import shlex
 wheel_cmd = shlex.split("python -m pip wheel . -v -w dist ")
 install_cmd = shlex.split("python -m pip install -e . -v")
 
+
 with open("build.log", "w") as log:
-    subprocess.run("cd build", stdout=log, stderr=log)
-    subprocess.run("cmake ..", stdout=log, stderr=log)
-    subprocess.run("make", stdout=log, stderr=log)
-
-time.sleep(2)
-
-with open("build.log", "a") as log:
     rv = subprocess.Popen(wheel_cmd, stdout=log, stderr=log)
     while rv.poll() is None:
         time.sleep(0.5)
