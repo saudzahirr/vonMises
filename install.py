0960cc7ebb4c1284a0c776073913cab4035e39e5
Saud Zahir <m.saud.zahir@gmail.com>
2024-03-28 15:43:20 +0500

    Fix

--- install.py
+++ install.py
@@ -5,22 +5,8 @@ import shlex
 
 wheel_cmd = shlex.split("python -m pip wheel . -v -w dist ")
 install_cmd = shlex.split("python -m pip install -e . -v")
-setup_directory = os.getcwd()
-
 
 with open("build.log", "w") as log:
-<<<<<<< HEAD
-=======
-    if not os.path.exists("build"):
-        os.makedirs("build")
-    os.chdir("build")
-    subprocess.run("cmake ..", stdout=log, stderr=log, shell=True)
-    subprocess.run("make", stdout=log, stderr=log, shell=True)
-
-os.chdir(setup_directory)
-
-with open("build.log", "a") as log:
->>>>>>> master
     rv = subprocess.Popen(wheel_cmd, stdout=log, stderr=log)
     while rv.poll() is None:
         time.sleep(0.5)
