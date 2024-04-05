0f10993b19e43c27be6bdb1309147b96efb942f3
Saud Zahir <m.saud.zahir@gmail.com>
2024-04-05 21:55:07 +0500

    Fix

--- src/eigen.py
+++ src/eigen.py
@@ -4,7 +4,7 @@ import time
 from pathlib import Path
 
 if sys.platform == "win32":
-    shared_file_path = Path(sys.prefix, "lib", "eigen.dll")
+    shared_file_path = Path(sys.prefix, "bin", "eigen.dll")
     eigen_lib = ctypes.CDLL(str(shared_file_path), winmode=0)
 elif sys.platform == "linux":
     shared_file_path = Path(sys.prefix, "lib", "libeigen.so")
